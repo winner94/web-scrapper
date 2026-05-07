268443579f4352a19206e97c87d27a9f3013ad4b
tkolecki <tkolecki@gmail.com>
2026-05-07 22:18:16 +0200

    feat: add saving to csv file

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 
 def print_rates(rates):
     for r in rates:
@@ -10,6 +11,14 @@ def find_rates(rates, code):
             return r
     return None
 
+def save_to_csv(rates):
+    with open("rates.csv", "w", newline="", encoding="utf-8") as csvfile:
+        fieldnames = ['code', 'currency', 'mid']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(rates)
+
+
 url = "http://api.nbp.pl/api/exchangerates/tables/A/?format=json"
 response = requests.get(url)
 
@@ -18,6 +27,7 @@ table = data[0]
 rates = table["rates"]
 
 
-currency = input("Enter currency code: ").upper()
-result = find_rates(rates, currency)
-print_rates([result]) if result else print("Not found")
+#currency = input("Enter currency code: ").upper()
+#result = find_rates(rates, currency)
+#print_rates([result]) if result else print("Not found")
+save_to_csv(rates)
