e9504cc09397efd44082f7b08c77094ae0f4fc2d
tkolecki <tkolecki@gmail.com>
2026-05-13 21:51:24 +0200

    feat: add plot generation

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,6 @@
 import requests
 import csv
+import matplotlib.pyplot as plt
 
 def print_rates(rates):
     for r in rates:
@@ -10,6 +11,22 @@ def find_rates(rates, code):
         if r['code'] == code:
             return r
     return None
+
+def plot_currency(code):
+    url = f"http://api.nbp.pl/api/exchangerates/rates/A/{code}/last/30/?format=json"
+    response = requests.get(url)
+    data = response.json()
+
+    dates = [r["effectiveDate"] for r in data["rates"]]
+    values = [r["mid"] for r in data["rates"]]
+
+    plt.plot(dates, values)
+    plt.title(f"{code} - last 30 days")
+    plt.xlabel("Date")
+    plt.ylabel("Exchange Rate")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
 
 def save_to_csv(rates):
     with open("rates.csv", "w", newline="", encoding="utf-8") as csvfile:
@@ -30,4 +47,8 @@ rates = table["rates"]
 #currency = input("Enter currency code: ").upper()
 #result = find_rates(rates, currency)
 #print_rates([result]) if result else print("Not found")
-save_to_csv(rates)
+#save_to_csv(rates)
+
+plot_currency("USD")
+plot_currency("EUR")
+plot_currency("CHF")
